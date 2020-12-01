2c55e1a3556ce6326768dcca2d71f37b0461fc9c
kinjal-111 <55944095+kinjal-111@users.noreply.github.com>
2020-12-01 16:20:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky custnavbar on scroll script
+        if(this.scrollY > 20){
+            $('.custnavbar').addClass("sticky");
+        }else{
+            $('.custnavbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.custnavbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/custnavbar script
+    $('.menu-btn').click(function(){
+        $('.custnavbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
 
 window.onscroll = function() {
     navBarFunction();
 }
 
-var navbar = document.getElementById("navbarJS");
+var custnavbar = document.getElementById("navbarJS");
 
 function navBarFunction() {
     if(window.scrollY >= 20) {
-        navbar.classList.add("sticky");
+        custnavbar.classList.add("sticky");
     } else {
-        navbar.classList.remove("sticky");
+        custnavbar.classList.remove("sticky");
     }
 }
 
@@ -47,3 +81,25 @@ function mygrade3() {
     var popup = document.getElementById("myPopup3");
     popup.classList.toggle("show");
 }
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
